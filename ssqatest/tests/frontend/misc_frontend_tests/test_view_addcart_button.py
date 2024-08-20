550dee968a199097f7022be87464dde82b4ce7e9
Latha <pai.latha1@gmail.com>
2024-08-20 17:38:39 -0500

    test for View Cart button

--- ssqatest/tests/frontend/misc_frontend_tests/test_view_addcart_button.py
+++ ssqatest/tests/frontend/misc_frontend_tests/test_view_addcart_button.py
@@ -1,13 +1,12 @@
 import  pytest
+import time
 import logging as logger
 from ssqatest.src.selenium_extended import SeleniumExtended
 from ssqatest.src.pages.HomePage import HomePage
-
 @pytest.mark.usefixtures("init_driver")
-@pytest.mark.lp012
 class TestAddCart:
     """
-        This is a test to ensure that when a user clicks on 'Add to Cart', the 'View cart' button is also displayed
+        This is a test to ensure that when a user clicks on 'Add to Cart', the 'View cart' button gets displayed
     """
     # create page objects
     @pytest.fixture(scope= 'class')
@@ -15,9 +14,15 @@ class TestAddCart:
         request.cls.homepage = HomePage(self.driver)
         yield
 
-    def test_add_button_displays_view_button(self):
+    @pytest.mark.lp012
+    def test_add_button_displays_view_button(self,setup):
         self.homepage.go_to_home_page()
         self.homepage.click_first_add_to_cart_button()
+        view_btn = self.homepage.get_view_button_first_product()
+
+        assert view_btn, f"'View cart' button does not show when the 'Add to cart' is clicked"
+        time.sleep(5)
+
 
 
 

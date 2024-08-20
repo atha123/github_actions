895d29cfaeab179fffd623f715ed609eba71a854
Latha <pai.latha1@gmail.com>
2024-08-20 10:47:00 -0500

    completed the test

--- ssqatest/tests/backend/misc_tests/test_update_a_product_price.py
+++ ssqatest/tests/backend/misc_tests/test_update_a_product_price.py
@@ -12,7 +12,6 @@ def test_update_price_of_product():
     """
     # Create objects that will run API call and also an object for the DAO
     api_helper = ProductsAPIHelper()
-    dao_helper = ProductsDAO()
 
     # Create a filter to pick items that are not on sale
     filters = {'on_sale':False, 'per_page': 100}
@@ -26,7 +25,7 @@ def test_update_price_of_product():
         item_id = selected_item['id']
         price_selected_item = selected_item["sale_price"]
         name_selected_item = selected_item["name"]
-        print(f" The sale price of selected item that is NOT on sale'{name_selected_item}'  before update is:  is: '{price_selected_item}'")
+        print(f" The sale price of selected item that is NOT on sale'{name_selected_item}'  before update is:  is: '{price_selected_item}' and ID is {item_id}")
     else: # Select random products if  all products are on sale.
         selected_item = random.choice(any_product)
         item_id = selected_item["id"]
@@ -36,13 +35,13 @@ def test_update_price_of_product():
 
     # For the selected item, update the price
     payload = dict()
-    breakpoint()
-    payload['regular_price'] = "10.00"
+    # breakpoint()
+    payload['regular_price'] = "15.00"
     update_price = api_helper.call_update_product(item_id,payload=payload)
-    print(f"The new updated dictionery is : '{update_price}'")
-    print(f"The new sale price of the selected item after update is : '{update_price['sale_price']}'")
-    print(f"The Regular Price is : {selected_item['regular_price']}")
-    print(f"The Price is : {selected_item['price']}")
+    # print(f"The new updated dictionery is : '{update_price}'")
+    # print(f"The Regular Price is : {update_price['regular_price']}")
+    # print(f"The Price is : {update_price['price']}")
 
     # Verify the price is updated correctly
-    # assert selected_item['sale_price'] == "10.00"
+    assert update_price['regular_price'] == "15.00", f"Regular Price was not updated. Expected was $'15.00' and Actual price is {update_price['regular_price']} "
+    assert update_price['price'] == "15.00", "Regular Price was not updated"

60fba10aaae0ef05e3b47a2962b83e67f2740dc7
Latha <pai.latha1@gmail.com>
2024-08-22 15:41:44 -0500

    script to test the add to cart button

---
+++ ssqatest/tests/frontend/misc_frontend_tests/test_add_cart_button.py
@@ -0,0 +1,31 @@
+import time
+import pytest
+from ssqatest.src.pages.HomePage import HomePage
+from ssqatest.src.pages.Header import Header
+import logging as logger
+
+
+@pytest.mark.usefixtures('init_driver')
+class TestAddCart:
+    """
+        This is a test to verify that when a user clicks on the 'Add to cart button', the cart gets incremented.
+    """
+    @pytest.fixture(scope='class')
+    def setup(self,request):
+        request.cls.home = HomePage(self.driver)
+        request.cls.header = Header(self.driver)
+
+    @pytest.mark.lp015
+    def test_add_cart(self,setup):
+        self.home.go_to_home_page()
+        count_before= self.header.get_cart_item_count()
+        logger.info(f"Count of items before clicking 'Add to cart' is : {count_before}")
+        self.home.click_first_add_to_cart_button()
+        self.header.wait_until_cart_item_count(1)
+        count_after = self.header.get_cart_item_count()
+        logger.info(f"Count of items after clicking 'Add to cart' is : {count_after}")
+
+        assert int(count_after) == int(count_before)+1, "failed"
+        logger.info("Test Passed!!")
+
+

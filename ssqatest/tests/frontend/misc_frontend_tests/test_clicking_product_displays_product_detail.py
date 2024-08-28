18032ec3e2287b078badf10f4c197d8ffbd8373a
Latha <pai.latha1@gmail.com>
2024-08-28 10:01:45 -0500

    updated tests per PR review

--- ssqatest/tests/frontend/misc_frontend_tests/test_clicking_product_displays_product_detail.py
+++ ssqatest/tests/frontend/misc_frontend_tests/test_clicking_product_displays_product_detail.py
@@ -2,10 +2,11 @@ import pytest
 from ssqatest.src.pages.ProductPage import ProductPage
 from ssqatest.src.pages.HomePage import HomePage
 import logging as logger
+
 @pytest.mark.usefixtures("init_driver")
 class TestClickingProductShowsDesc:
     """
-        This is a test to ensure that when the user clicks on the product, the product description gets displayed.
+    This is a test to ensure that when the user clicks on the product, the product description gets displayed in the details page.
     """
     @pytest.fixture(scope='class')
     def setup(self,request):
@@ -14,8 +15,11 @@ class TestClickingProductShowsDesc:
 
     @pytest.mark.lp014
     def test_product_name_under_image(self,setup):
+        # breakpoint()
         self.home.go_to_home_page()
+        first_product = self.home.get_firts_product_name()
+        # print(f"first product is : '{first_product}'")
         self.home.click_first_product_image()
         name = self.product.get_displayed_product_name()
-        assert name == 'Album', "Product details page does not show the name of the Product"
+        assert name == first_product, "Product details page does not show the name of the Product"
         logger.info("Test Passed! ")
--- ssqatest/tests/frontend/misc_frontend_tests/test_product_name_under_image.py
+++ ssqatest/tests/frontend/misc_frontend_tests/test_product_name_under_image.py
@@ -1,11 +1,11 @@
 import pytest
 from ssqatest.src.pages.HomePage import HomePage
-# from ssqatest.src.selenium_extended import SeleniumExtended
+import logging as logger
 
 @pytest.mark.usefixtures("init_driver")
 class TestProductName:
     """
-    This is a test to ensure that the the product description shows up under the product image.
+    This is a test to ensure that the the product name shows up under the product image.
     """
 
     # Creating page objects
@@ -17,7 +17,10 @@ class TestProductName:
     @pytest.mark.lp013
     def test_product_desc(self,setup):
         self.homepage.go_to_home_page()
-        first_product = self.homepage.get_firts_product_name()
-        assert first_product == 'Album', f"Product name displayed is not 'Album'. Actual displayed is: '{first_product}'"
+        first_product = self.homepage.get_product_name()
+        assert first_product.is_displayed(), f"Product name displayed is not 'Album'. Actual displayed is: '{first_product}'"
+
+        logger.info("Test Passed !")
+
 
 

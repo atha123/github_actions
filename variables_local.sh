5ac963a5bb354460c6d7604b61a6cc659d867244
latha <pai.latha1@gmail.com>
2024-10-31 14:06:42 -0500

    updated variables.sh BASE_URL

--- variables_local.sh
+++ variables_local.sh
@@ -2,7 +2,8 @@
 
 
 #export BASE_URL=http://192.168.0.110:8888/localdemostore/
-export BASE_URL=http://127.0.0.1:8888/localdemostore/
+#export BASE_URL=http://127.0.0.1:8888/localdemostore/
+export BASE_URL=http://dev.bootcamp.store.supersqa.com
 export BROWSER=chrome
 export RESULTS_DIR=$(pwd)/results
 export DB_PORT=8889

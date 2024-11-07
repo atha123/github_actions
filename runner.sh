1a1a4fd5408351c1b0d1521b37c652f376ea6948
latha <pai.latha1@gmail.com>
2024-11-07 12:00:03 -0600

    hardcoded the path

--- runner.sh
+++ runner.sh
@@ -1,17 +1,12 @@
-set -x
+#set -x
 
 #location = $1
 #options = $2
 current_date=$(date '+%Y-%m-%d')
-echo pwd
-#cd ssqatest
-python3 -m pytest ssqatest/tests/backend --html=../../results/${current_date}.html  --self-contained-html
 
+python3 -m pytest ssqatest/tests/backend --html=../../results/${current_date}.html  --self-contained-html
 #python3 -m pytest ${location}  --html=../results/${current_date}.html  --self-contained-html
 echo "This is from runner file- Tests have run through the runner file"
-
-
-
 
 
 # ORIGINAL RUNNER.SH FILE. THE LINES ABOVE THIS LINE ARE ADDED BY LHP

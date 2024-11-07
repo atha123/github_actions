4ca3a359ed395c4d4aeb05844c3196ba9bcb50a0
latha <pai.latha1@gmail.com>
2024-11-06 20:28:23 -0600

    commit both files

--- runner.sh
+++ runner.sh
@@ -1,13 +1,14 @@
 set -x
 
-location = $1
+#location = $1
 #options = $2
 current_date=$(date '+%Y-%m-%d')
 
 
 #python3 -m ${location} --html=../../results/${current_date}.html  --self-contained-html
 pwd
-python3 -m pytest ${location} --html=../../results/${current_date}.html  --self-contained-html
+cd ssqatest
+python3 -m pytest ssqatest/tests/backend  --html=../results/${current_date}.html  --self-contained-html
 echo "This is from runner file- Tests have run through the runner file"
 
 

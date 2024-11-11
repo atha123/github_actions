83cc78ccc4143c03359afe99440ee95d072aeae1
latha <pai.latha1@gmail.com>
2024-11-11 11:19:22 -0600

    uncommenting the location variable. Location is now passed as an argument with the runner file in the yml file

--- runner.sh
+++ runner.sh
@@ -1,11 +1,11 @@
 set -x
 
-#location = $1
+location = $1
 #options = $2
 current_date=$(date '+%Y-%m-%d')
 
-python3 -m pytest ssqatest/tests/backend --html=../../results/${current_date}.html  --self-contained-html
-#python3 -m pytest ${location}  --html=../results/${current_date}.html  --self-contained-html
+#python3 -m pytest ssqatest/tests/backend --html=../../results/${current_date}.html  --self-contained-html
+python3 -m pytest ${location}  --html=../results/${current_date}.html  --self-contained-html
 echo "This is from runner file- Tests have run through the runner file"
 
 
